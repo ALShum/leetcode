50e16c87d012b4c8bc661c55e76b5f7e15ee49d6
ashum <alex@alshum.com>
2020-04-22 00:45:51 -0700

    1234

--- 0028-implement_strstr.py
+++ 0028-implement_strstr.py
@@ -1,23 +1,21 @@
 class Solution:
-    ## Manual
-    def strStr2(self, haystack: str, needle: str) -> int:
+    # For loop and then forward scan
+    def strStr(self, haystack: str, needle: str) -> int:
         if not needle:
             return 0
 
-        scan_pos = 0
-        while scan_pos < len(haystack):
+        n = len(needle)
+        for scan_pos in range(0, len(haystack)):
             if haystack[scan_pos] == needle[0]:
-                n = len(needle)
                 if (scan_pos + n - 1) < len(haystack):
                     if haystack[scan_pos:scan_pos + n] == needle:
                         return scan_pos
                 else:
                     break
-            scan_pos += 1
         return -1
 
-    ## Using python string method
-    def strStr(self, haystack: str, needle: str) -> int:
+    # Cheat with python str methods
+    def strStr2(self, haystack: str, needle: str) -> int:
         try:
             return haystack.index(needle)
         except:
