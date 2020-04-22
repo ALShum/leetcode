48f0da14611fa242b2f68a15e393f63e2f1a0bd1
ashum <alex@alshum.com>
2020-04-22 00:41:34 -0700

    1234

---
+++ 0344-reverse_string.py
@@ -0,0 +1,20 @@
+class Solution:
+    ## Two pointer method
+    def reverseString2(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        l = 0
+        r = len(s)-1
+        while l < r:
+            s[l], s[r] = s[r], s[l]
+            l+=1
+            r-=1
+
+    ## For loop method
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        for i in range(len(s) // 2):
+            s[i], s[len(s) - i - 1] = s[len(s) - i - 1], s[i]
